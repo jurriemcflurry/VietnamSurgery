787c8aac422b1a62d86b1570d600ec84e99fd250
Summergirl012 <sjmprick@gmail.com>
2018-12-30 21:58:54 +0100

    Created scrollview when filling stuff in. Made the camera save the pics as JPEG full size and thumb size png

--- app/src/main/java/toning/juriaan/vietnamsurgery/CameraActivity.java
+++ app/src/main/java/toning/juriaan/vietnamsurgery/CameraActivity.java
@@ -3,21 +3,25 @@ package toning.juriaan.vietnamsurgery;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,7 +46,6 @@ public class CameraActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap mImageBitmap;
     private GridLayout gridLayout1;
-    private Button saveImagesButton;
     private ArrayList<Bitmap> mImages = new ArrayList<>();
     private FormTemplate form;
 
@@ -57,8 +60,7 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_activity);
 
-        gridLayout1 = (GridLayout) findViewById(R.id.gridLayout1);
-        //saveImagesButton = (Button) findViewById(R.id.save_images);
+        gridLayout1 = findViewById(R.id.gridLayout1);
         Intent i = getIntent();
         form = i.getParcelableExtra("obj_form");
 
@@ -102,10 +104,7 @@ public class CameraActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                dispatchTakePictureIntent();
             }
         });
 
@@ -127,20 +126,70 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "toning.juriaan.vietnamsurgery.fileprovider",
+                        photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name - At this moment we're using the name, birthday & dateTimeformat
+        // Todo: Create a possibility to get a name out of the Excel or let the user choose.
+        String patientName = form.getSections().get(0).getFields().get(1).getAnswer();
+        String birthYear = form.getSections().get(0).getFields().get(2).getAnswer();
+        String dateStamp = new SimpleDateFormat("yyyyMMdd_HH:mm:ss").format(new Date());
+        String imageFileName = patientName + "_" + birthYear + "_" + dateStamp;
+        File storageDir = new File(Environment.getExternalStorageDirectory().toString() + "/LenTab/lentab-susanne/VietnamSurgery");
+        File image = new File(storageDir, imageFileName + ".jpg");
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     //het resultaat van de camera (een foto) wordt hier in een nieuwe ImageView gestopt
     //de imageview wordt toegevoegd aan een gridlayout
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            mImageBitmap = (Bitmap) extras.get("data");
+            File file = new File(mCurrentPhotoPath);
             ImageView imageView = new ImageView(CameraActivity.this);
-            imageView.setImageBitmap(mImageBitmap);
-            gridLayout1.addView(imageView);
-            imageView.getLayoutParams().height = (getDisplayMetrics().heightPixels)/2;
-            imageView.getLayoutParams().width = (getDisplayMetrics().widthPixels)/2;
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            mImages.add(mImageBitmap);
+            int thumbSize = 400;
+            try {
+                Bitmap thumb = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(mCurrentPhotoPath), thumbSize, thumbSize);
+                /*Bitmap bitmap = MediaStore.Images.Media
+                        .getBitmap(this.getContentResolver(), Uri.fromFile(file));*/
+                imageView.setImageBitmap(thumb);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                lp.setMargins(10, 10, 10, 10);
+                imageView.setLayoutParams(lp);
+                gridLayout1.addView(imageView);
+                mImages.add(thumb);
+
+            } catch (Exception ex) {
+                Log.i("TESTT", "I made a fkup");
+            }
         }
     }
 
@@ -166,14 +215,14 @@ public class CameraActivity extends AppCompatActivity {
             try {
                 newFileName = filename + "_" + String.valueOf(i);
                 String root = Environment.getExternalStorageDirectory().toString();
-                File myDir = new File(root + File.separator + "/LenTab/lentab-susanne/VietnamSurgery");
+                File myDir = new File(root + File.separator + "/LenTab/lentab-susanne/VietnamSurgery/thumbs");
                 File mypath = null;
                 if(!myDir.exists()) {
                     if(myDir.mkdirs()){
-                        mypath = new File(myDir, filename + ".png");
+                        mypath = new File(myDir, newFileName + ".png");
                     }
                 } else {
-                    mypath = new File(myDir, filename + ".png");
+                    mypath = new File(myDir, newFileName + ".png");
                 }
 
                 FileOutputStream fos = new FileOutputStream(mypath);
@@ -199,7 +248,7 @@ public class CameraActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 // this takes the user 'back', as if they pressed the left-facing
-                form.setBitmapImages(mImages);
+                //form.setBitmapImages(mImages);
                 Intent i = new Intent(getApplicationContext(), FormActivity.class);
                 i.putExtra("obj_form", form);
                 i.putExtra("step", noOfSections);
