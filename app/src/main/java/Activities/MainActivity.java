b3821247a8c3e9cda52ffa0f71b9cdb2c25f159c
jurriemcflurry <45285489+jurriemcflurry@users.noreply.github.com>
2019-01-08 18:24:32 +0100

    Menu Bug Fix

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -42,7 +42,6 @@ import toning.juriaan.Models.Storage;
 
 public class MainActivity extends BaseActivity implements FormAdapter.FormListener {
 
-    private DrawerLayout mDrawerLayout;
     private RecyclerView recyclerView;
     private FormAdapter formAdapter;
     private ArrayList<Form> forms;
@@ -53,7 +52,10 @@ public class MainActivity extends BaseActivity implements FormAdapter.FormListen
         //thema moet altijd worden gezet naar AppTheme, zodat de Launcher van het splashscreen niet bij elke actie wordt getoond
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        getLayoutInflater().inflate(R.layout.activity_main, contentFrameLayout);
+        getSupportActionBar().setTitle(getString(R.string.home));
 
         forms = new ArrayList<>();
         recyclerView = findViewById(R.id.form_recycler);
@@ -61,12 +63,6 @@ public class MainActivity extends BaseActivity implements FormAdapter.FormListen
         recyclerView.setAdapter(formAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
-        getLayoutInflater().inflate(R.layout.activity_main, contentFrameLayout);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(getString(R.string.home));
-        }
 
         Button OpenCamera = (Button) findViewById(R.id.ToCamera);
         OpenCamera.setOnClickListener(new View.OnClickListener() {
@@ -136,89 +132,6 @@ public class MainActivity extends BaseActivity implements FormAdapter.FormListen
     public void updateView() {
         formAdapter.setForms(forms);
         formAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void setupNavigation() {
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        View headerView = navigationView.getHeaderView(0);
-        LinearLayout header = (LinearLayout) headerView.findViewById(R.id.headerlayout);
-        final TextView login = (TextView) header.findViewById(R.id.Logintext);
-        final TextView loggedInUser = (TextView) header.findViewById(R.id.LoggedinUser);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        if (AccessToken.access_token != null) {
-            login.setText(getString(R.string.logout));
-            loggedInUser.setText(AccessToken.userName);
-        } else {
-            login.setText(getString(R.string.login));
-            loggedInUser.setText(getString(R.string.not_logged_in));
-        }
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (login.getText().equals(getString(R.string.logout))) {
-                    AccessToken.access_token = null;
-                    AccessToken.userName = null;
-                    login.setText(getString(R.string.login));
-                    loggedInUser.setText(getString(R.string.not_logged_in));
-                    mDrawerLayout.closeDrawers();
-                } else {
-                    // ga naar pagina om in te loggen
-                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(loginIntent);
-                }
-            }
-        });
-
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
-                        switch (menuItem.getItemId()) {
-                            case R.id.nav_1: //Bovenste Item
-                                Intent naarForms = new Intent(MainActivity.this, FormActivity.class);
-                                startActivity(naarForms);
-                                break;
-//                            case R.id.nav_2: //2e item
-//                                Intent naarUsers = new Intent(MainActivity.this, UsersActivity.class);
-//                                startActivity(naarUsers);
-//                                break;
-//                            case R.id.nav_3: //3e item
-//                                break;
-//                            case R.id.nav_4: //4e item
-//                                break;
-                            default:
-                                break;
-                        }
-
-                        return true;
-                    }
-                });
     }
 
     @Override
